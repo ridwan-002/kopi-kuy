694754bfd6056b45302b884098c6f95d7f20f785
ridwan-002 <ridwansisoleh795@gmail.com>
2024-05-11 09:41:21 +0700

    first commit

---
+++ js/produk.js
@@ -0,0 +1,45 @@
+var data_mentah = {
+    "Arabica": [
+        {
+            "foto": "images/produk/product_capuchino.jpg",
+            "nama": "Cappuccino",
+            "size": "250 Ml",
+            "harga": "25.000",
+            "link": "https://tokopedia.com/produk/cappucciono1"
+        },
+        {
+            "foto": "images/produk/product_espresso.jpg",
+            "nama": "Espresso",
+            "size": "100 Ml",
+            "harga": "15.000",
+            "link": "https://tokopedia.com/produk/espresso"
+        },
+        {
+            "foto": "images/produk/product_kopi_susu.jpg",
+            "nama": "Kopi Susu",
+            "size": "200 Ml",
+            "harga": "20.000",
+            "link": "https://tokopedia.com/produk/kopisusu"
+        }
+    ],
+    "Robusta": [
+        {
+            foto: "images/produk/product_latte.jpg",
+            nama: "Latte",
+            size: "200 Ml",
+            harga: "20.000",
+            link: "https://tokopedia.com/produk/latte"
+        }
+    ],
+    "Non Kopi": [
+        {
+            foto: "https://www.sasa.co.id/medias/page_medias/es_cendol_nangka.jpg",
+            nama: "Es Cendol Nangka",
+            size: "200 Ml",
+            harga: "15.000",
+            link: "https://tokopedia.com/produk/escendol+nangka"
+        }
+    ]
+}
+
+console.log("Isi Data mentah:", data_mentah)
